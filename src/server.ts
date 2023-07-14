82629a79b2cac73d177de2eb1379eef41db2e27d
Juan <juan_jg@hotmail.com>
2023-07-14 20:36:50 -0300

    added debug config

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,16 @@ import express from 'express';
 const app = express();
 const port = 3335;
 
-app.get('/', (req, res) => res.json({ message: 'Hello World' }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello MuGoL' });
+});
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+  return res.json({ name });
+});
 
 app.listen(port, () => {
   console.log(`server on http://localhost:${port}`);
