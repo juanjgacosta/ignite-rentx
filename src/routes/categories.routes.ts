0ba3199f26b7fd5b5751d13a9a2e79f10a709c10
Juan <juan_jg@hotmail.com>
2023-08-01 23:47:45 -0300

    added categories.routes module

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+
+  const category = {
+    id: uuidv4(),
+    name,
+    description,
+  };
+
+  categories.push(category);
+  console.log(category);
+
+  return res.status(201).json(category);
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 const port = 3335;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello MuGoL' });
-});
-
-app.post('/courses', (req, res) => {
-  const { name } = req.body;
-  return res.json({ name });
-});
+app.use('/categories', categoriesRoutes);
 
 app.listen(port, () => {
   console.log(`server on http://localhost:${port}`);
