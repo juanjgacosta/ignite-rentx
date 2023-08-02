203e63802392b74cabb1beaaaf764a9de8eb732d
Juan <juan_jg@hotmail.com>
2023-08-01 23:56:45 -0300

    added model Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
+
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
-    id: uuidv4(),
+  const category: Category = {
+    id: uuidV4(),
     name,
     description,
+    created_at: new Date(),
   };
 
   categories.push(category);
