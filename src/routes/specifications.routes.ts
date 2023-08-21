f6d524f99e0da80420ee4186291ca52bbbce3a99
Juan <juan_jg@hotmail.com>
2023-08-21 13:18:50 -0300

    added useCase de specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,26 +1,16 @@
 import { Router } from 'express';
 
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post('/', (req, res) => {
-  const { name, description } = req.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository,
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return res.status(201).send();
+  return createSpecificationController.handle(req, res);
 });
 
-specificationsRoutes.get('/', (req, res) => {
-  const all = specificationsRepository.list();
-  return res.json(all);
-});
+// specificationsRoutes.get('/', (req, res) => {
+//   const all = specificationsRepository.list();
+//   return res.json(all);
+// });
 
 export { specificationsRoutes };
