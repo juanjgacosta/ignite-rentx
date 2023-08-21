9a0fbdfbfafc96741326f2bc5ae8c8c9503e1c08
Juan <juan_jg@hotmail.com>
2023-08-21 13:31:38 -0300

    added feature para listagem de specifications - pendente implementar instancias separadas do repositório

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
+import { listSpecificationsController } from '../modules/cars/useCases/listSpecifications';
 
 const specificationsRoutes = Router();
 
@@ -8,9 +9,8 @@ specificationsRoutes.post('/', (req, res) => {
   return createSpecificationController.handle(req, res);
 });
 
-// specificationsRoutes.get('/', (req, res) => {
-//   const all = specificationsRepository.list();
-//   return res.json(all);
-// });
+specificationsRoutes.get('/', (req, res) => {
+  return listSpecificationsController.handle(req, res);
+});
 
 export { specificationsRoutes };
