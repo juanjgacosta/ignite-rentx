94732ba38c7bfa9ab19b45d2a045b0b0b478b48e
Juan <juan_jg@hotmail.com>
2023-08-23 19:23:55 -0300

    added documentacao api com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 const port = 3335;
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(port, () => {
