4f897d87d1a6af913a203b02afd23958c062462d
Juan <juan_jg@hotmail.com>
2023-09-01 11:40:14 -0300

    update database/index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: 'database_ignite',
   port: 5432,
   username: 'docker',
   password: 'ignite',
