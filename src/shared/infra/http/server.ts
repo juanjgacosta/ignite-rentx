24b98b70ce97f200451c9222d75e8c7174e80a20
Juan <juan_jg@hotmail.com>
2025-01-07 13:12:03 -0500

    add user admin seed setup

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,16 +3,15 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-// import { AppError } from './errors/AppError';
 import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
 
-import '@shared/infra/typeorm';
-// import './shared/container';
 import '@shared/container';
 
+createConnection();
 const app = express();
 const port = 3335;
 
