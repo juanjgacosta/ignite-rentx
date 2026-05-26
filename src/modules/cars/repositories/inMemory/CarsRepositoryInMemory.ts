0f22bdd9b7220c0d8d499be3080300b777eb30d4
Juan <juan_jg@hotmail.com>
2026-05-25 20:42:10 -0500

    implement ListCarsUseCase and update findAvailable method in CarsRepositoryInMemory

--- src/modules/cars/repositories/inMemory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/inMemory/CarsRepositoryInMemory.ts
@@ -42,18 +42,18 @@ class CarsRepositoryInMemory implements ICarsRepository {
     category_id?: string,
     name?: string,
   ): Promise<Car[]> {
-    const cars = this.cars.filter((car) => {
+    const all = this.cars.filter((car) => {
       if (
-        car.available === true &&
-        ((brand && car.brand === brand) ||
-          (category_id && car.category_id === category_id) ||
-          (name && car.name === name))
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
       ) {
-        return cars;
+        return car;
       }
       return null;
     });
-    return cars;
+    return all;
   }
 }
 
