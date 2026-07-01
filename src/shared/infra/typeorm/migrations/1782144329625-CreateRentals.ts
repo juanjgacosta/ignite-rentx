c485756dc0455a4044fcf91894b981fc934cb478
Juan <juan_jg@hotmail.com>
2026-07-01 10:48:17 -0500

    refactor: remove unused import for TableForeignKey in CreateRentals migration

--- src/shared/infra/typeorm/migrations/1782144329625-CreateRentals.ts
+++ src/shared/infra/typeorm/migrations/1782144329625-CreateRentals.ts
@@ -1,9 +1,4 @@
-import {
-  MigrationInterface,
-  QueryRunner,
-  Table,
-  TableForeignKey,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateRentals1782144329625 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
